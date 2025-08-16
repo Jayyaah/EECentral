5d1ae096de0effd141205f8619b473b36b05469c
Jayyaah <valentines.maitre@gmail.com>
2025-08-16 11:03:40 +0200

    init project

---
+++ src/main/java/com/jayyaah/eecentral/EeCentralApplication.java
@@ -0,0 +1,13 @@
+package com.jayyaah.eecentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeCentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EeCentralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jayyaah/eecentral/EeCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayyaah.eecentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeCentralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
