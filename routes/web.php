c798cf9806ce9a72289bbab64b9f7c731a62cc1f
Jayyaah <valentines.maitre@gmail.com>
2026-01-11 09:55:31 +0100

    Add admin route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ArticleController as AdminArticleController;
+use App\Http\Controllers\Front\ArticleController as FrontArticleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('articles', AdminArticleController::class);
+});
+
+Route::get('/', [FrontArticleController::class, 'index']);
+Route::get('/articles/{slug}', [FrontArticleController::class, 'show']);
