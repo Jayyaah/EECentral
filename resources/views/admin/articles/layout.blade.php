2dba571fc83ce5ae52b16510de85c3adda1dfa69
Jayyaah <valentines.maitre@gmail.com>
2026-01-11 10:10:49 +0100

    Create layout.blade.php

---
+++ resources/views/admin/articles/layout.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+</head>
+<body>
+<nav>
+    <a href="{{ route('admin.articles.index') }}">Articles</a>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button>Déconnexion</button>
+    </form>
+</nav>
+
+<hr>
+
+@yield('content')
+</body>
+</html>
