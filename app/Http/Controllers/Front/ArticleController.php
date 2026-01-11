5dbeae21560260ed85f0ea4251a0ce7b03b75e00
Jayyaah <valentines.maitre@gmail.com>
2026-01-11 10:15:32 +0100

    Add index

--- app/Http/Controllers/Front/ArticleController.php
+++ app/Http/Controllers/Front/ArticleController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Front OK (temporaire)';
+    }
 }
+
