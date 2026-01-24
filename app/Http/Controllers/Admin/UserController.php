7a67884e3a8b158dcfee258c20dbe369c73fe638
Jayyaah <valentines.maitre@gmail.com>
2026-01-24 17:17:13 +0100

    Create UserController.php

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
+
+class UserController extends Controller
+{
+    public function index() {
+        $users = User::latest()->get();
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function create() {
+        return view('admin.users.create');
+    }
+
+    public function store(Request $request) {
+        $password = Str::random(12);
+
+        $validated = request()->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|min:8',
+            'role' => 'required|in:admin,editor',
+        ]);
+
+        User::create([
+           'name' => $validated['name'],
+           'email' => $validated['email'],
+           'password' => Hash::make($password),
+           'role' => $validated['role'],
+        ]);
+
+        Mail::to($user->email)->send(
+            new NewUserAccountMail($user, $password)
+        );
+
+        return redirect()
+            ->route('admin.users.index')
+            ->with('success', 'Utilisateur créé');
+    }
+}
