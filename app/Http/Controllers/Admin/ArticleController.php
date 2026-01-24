0937d5ebdceff549ca051abd48e76b080f27ac01
Jayyaah <valentines.maitre@gmail.com>
2026-01-24 15:57:59 +0100

    Update ArticleController.php

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -113,8 +113,4 @@ class ArticleController extends Controller
             ->route('admin.articles.index')
             ->with('success', 'Article supprimé');
     }
-
-    public function __construct(){
-        $this->authorizeResource(Article::class, 'article');
-    }
 }
