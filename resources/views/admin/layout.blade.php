202de208fbb07efdf185d44b48b97f51dc36e7cf
Jayyaah <valentines.maitre@gmail.com>
2026-01-24 12:30:47 +0100

    Update layout.blade.php

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -14,6 +14,11 @@
 </nav>
 
 <hr>
+@if(session('success'))
+    <div style="background:#d1fae5; padding:10px; margin-bottom:10px;">
+        {{ session('success') }}
+    </div>
+@endif
 
 @yield('content')
 </body>
