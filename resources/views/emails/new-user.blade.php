001c392dc980dd63c5a229b223d8013b99ed8ab4
Jayyaah <valentines.maitre@gmail.com>
2026-01-24 17:17:17 +0100

    Create new-user.blade.php

---
+++ resources/views/emails/new-user.blade.php
@@ -0,0 +1,18 @@
+<h1>Bienvenue sur EE Central</h1>
+
+<p>Bonjour {{ $user->name }},</p>
+
+<p>Un compte vient d’être créé pour vous.</p>
+
+<p><strong>Identifiant :</strong> {{ $user->email }}</p>
+<p><strong>Mot de passe temporaire :</strong> {{ $password }}</p>
+
+<p>
+    <a href="{{ url('/login') }}">
+        👉 Se connecter
+    </a>
+</p>
+
+<p>
+    Pour des raisons de sécurité, merci de changer votre mot de passe après connexion.
+</p>
