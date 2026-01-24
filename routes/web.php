5eeec12860b6c1d2137249337c477b8a001f6bce
Jayyaah <valentines.maitre@gmail.com>
2026-01-24 13:30:29 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ArticleController;
@@ -10,14 +11,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/**
+ * Dashboard Breeze
+ */
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/**
+ * Admin
+ */
 Route::middleware(['auth'])
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('articles', ArticleController::class);
     });
 
